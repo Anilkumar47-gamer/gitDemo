a84490463860f40fce5f461545dba10fcaa43913
anilak0987@gmail.com <anilak0987@gmail.com>
2024-01-24 23:26:41 +0530

    print main method once again

--- src/gitDemo/test/TestGit.java
+++ src/gitDemo/test/TestGit.java
@@ -3,5 +3,8 @@ package gitDemo.test;
 public class TestGit {
 public static void main(String[] args) {
 	System.out.println("hi this is main method");
+	System.out.println("hi this is main method");
+
 }
+
 }
